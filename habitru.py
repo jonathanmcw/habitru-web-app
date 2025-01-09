ae69b1a0655ffa0bfe2f105a76b0141d3e8680bf
Jonathan Wong <jonathanmcw@gmail.com>
2025-01-09 17:14:17 +0800

    Working on Yearly View

--- habitru.py
+++ habitru.py
@@ -9,24 +9,32 @@ app = Flask(__name__)
 # > "what habit or activities would you like to tracK?" 
 #       [ ] need to have couple more options - time to ask around to brainstorm
 #       [ ] Idea: keep a notebook myself to see what kind of things can / should be quantified
-#       [ ] Both Physical, Emotional, Social
+#       [NEXT] Both Physical, Emotional, Social
 # > Home screen
-#       [ ] Keep it very simple with as little fiddling as possible.
-#           - Click to mark item as done
-#           - Long press to mark something not done done
-#           - Tap into item for details
-#           - Tactile - like a bubble sheet + sound
-#       [ ] Button row to add new item 
-#           - Name
-#           - Description
-#           - Quantity / Unit ( affects the type of UI to be generated )
-#           - Frequency / start / end date
+#       [ ] Show only habit that are not done today
+#       [ ] Go to next date 
+#       [ ] ?? Mark a task back to "not done?"
+#       [DONE] Keep it very simple with as little fiddling as possible.
+#           [DONE] Click to mark item as done
+#           [NEXT] Long press to mark something not done done
+#           [DONE] Tap into item for details
+#           [NEXT] Tactile - like a bubble sheet + sound
+#       [DONE] Button row to add new item 
+#           [DONE] Name
+#           [NEXT] Description
+#           [NEXT] Quantity / Unit ( affects the type of UI to be generated )
+#           [DONE] Frequency / start / end date 
+#           [NEXT] end date
+#       
 # > monthly-view screen
-#       [ ] - Emotionally, make it very satisfying to look at :) like a proper achievement
-#       [ ] My stats and streaks :) 
+#       [NEXT] - Emotionally, make it very satisfying to look at :) like a proper achievement
+#       [NEXT] My stats and streaks :) 
 #           - any sort of insights I could dig from there
+# > Year screen
+#       [ ] Convert date into 365 and then show
+#       [ ] Show done item for the year 
 # > Profile
-#       [ ] Consider what are the needs, and whether we do need to have a family account to share success
+#       [LATER] Consider what are the needs, and whether we do need to have a family account to share success
 # > Why people procrastinate ? 
 
 # Ensure no caching
@@ -48,26 +56,44 @@ def welcome():
     conn = get_db_connection()
     habits = conn.execute('SELECT * FROM habits').fetchall()
     conn.close()
-    # TODO: Based on what the option that I have selected, the options should be shown to be in Today
-    return render_template('welcome.html', habits = habits, current_url=request.path)
-    # return "<html><body>what exercise would you like to track?</body></html>"
+    print("Fetched habits:", habits)  # Debug print statement
+    return render_template('welcome.html', habits=habits, current_url=request.path)
 
 @app.route("/daily_view", methods=["GET","POST"])
 def daily_view():
-    conn = get_db_connection()
-    habits = conn.execute('SELECT * FROM habits').fetchall()
 
-    # if there is a new habit
-    # new_habit = request("new-tracking")
-    
-    conn.close()
     # TODO: Enable user to switch day 
     # TODO: How should I design my database to have task of different dates? 
     current_time = datetime.now().strftime("%-d %b %Y")
+
     if request.method == "POST":
+        conn = get_db_connection()
+
+        # ??? how do I render the daily, monthly and yearly list?
+        # ??? daily - SELECT * FROM records or progress (???) WHERE values is zero ? 
+        # ??? NOTE: Method 2 >>> Where today needs to do something but record shows not found ... >>> just join table 
+        #    
+        # TODO:
+        # habit_id = request.form.get("habit-id")
+        # conn.execute("UPDATE ")
+        # conn.execute("INSERT INTO records (habit_id, done) VALUES (?, ?) ", habit_id ,1)
+            # return render_template('daily_view.html', habits = habits, current_time = current_time, current_url=request.path )
         
+        # <p>Current version: type: "to_do" value: "none" start_date: "today", frequency: "daily"</p>
+
+        # COULD BE ADDING NEW
+        habit_name = request.form.get("habit-name")
+        # TODO: validation
+        conn.execute("INSERT INTO habits (name, type, value, description, start_date, frequency) VALUES (?, ?, ?, ?, ?, ?)", (habit_name, "to_do", "none", "empty", "2025-01-09", "daily"))
+        conn.commit()
+        conn.close()
+
         return redirect(url_for('daily_view'))
     
+    conn = get_db_connection()
+
+    habits = conn.execute('SELECT * FROM habits').fetchall()
+    conn.close()
     return render_template('daily_view.html', habits = habits, current_time = current_time, current_url=request.path )
 
 # Update / Add view
@@ -80,8 +106,8 @@ def monthly_view():
     habits = conn.execute('SELECT * FROM habits').fetchall()
     conn.close()
     
-    dates = [];
-    months = [];
+    dates = []
+    months = []
 
     return render_template('monthly_view.html', habits=habits, current_time = current_time, dates=dates, months = months, current_url=request.path)
     # return render_template('monthly-view.html', transposed_data=transposed_data, habits=habits, dates=dates)
@@ -93,12 +119,21 @@ def yearly_view():
 
     conn = get_db_connection()
     habits = conn.execute('SELECT * FROM habits').fetchall()
+    habit_records = conn.execute('''SELECT records.date, records.habit_id, habits.name FROM records 
+                                    JOIN habits ON records.habit_id = habits.id 
+                                    WHERE date LIKE '2025%'  
+                                    ORDER BY habits.id''').fetchall()
     conn.close()
 
-    dates = [f"{i+1:02d}" for i in range(30)]
-    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    def date_to_day_of_year(date_str):
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+        return date_obj.timetuple().tm_yday
+
+    habit_records = [{"day_of_year": date_to_day_of_year(record["date"]), 
+                      "habit_id": record["habit_id"], 
+                      "name": record["name"]} for record in habit_records]
     
-    return render_template('yearly_view.html', habits=habits, current_time=current_time, dates=dates, months=months, current_url=request.path )
+    return render_template('yearly_view.html', current_time=current_time, habits=habits, habit_records=habit_records, current_url=request.path)
 
 @app.route("/habit_details", methods=["GET"])
 def habit_details():    
@@ -111,6 +146,7 @@ def habit_details():
     habit_info = {}
 
     if habit_row:
+        habit_info["id"] = habit_row["id"]
         habit_info["habit_name"] = habit_row["name"]
         habit_info["type"] = habit_row["type"]
         habit_info["value"] = habit_row["value"]
@@ -121,12 +157,11 @@ def habit_details():
 def new_habit():
     return render_template('new_habit.html')
 
-
-
 @app.route("/mark_done/<int:habit_id>", methods=["POST"])
 def mark_done(habit_id):
     conn = get_db_connection()
-    conn.execute('UPDATE habits SET done = 1 WHERE id = ?;', (habit_id,))
+    # conn.execute('UPDATE habits SET done = 1 WHERE id = ?;', (habit_id,))
+    conn.execute('INSERT INTO records (habit_id, date, status) VALUES (?, "2025-01-09", "done");', (habit_id,))
     conn.commit()
     conn.close()
     return '', 204 # No content
--- init_db.py
+++ init_db.py
@@ -2,77 +2,55 @@ import sqlite3
 
 def init_db():
     conn = sqlite3.connect('habitru.db')
-    c = conn.cursor()
+    try:
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS habits (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                type TEXT NOT NULL,
+                value TEXT NOT NULL, 
+                description TEXT,
+                start_date DATE NOT NULL, 
+                frequency TEXT DEFAULT 'daily'
+            )          
+        ''')
+        print("Created table 'habits'")
 
-    # Create table
-    # c.execute('''
-    #     CREATE TABLE IF NOT EXISTS habits (
-    #           id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #           name TEXT NOT NULL,
-    #           type TEXT NOT NULL,
-    #           value TEXT NOT NULL,
-    #           done INTEGER DEFAULT 0,
-    #           description TEXT
-    #     )          
-    # ''')
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS records (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                habit_id INTEGER NOT NULL,
+                date DATE NOT NULL,
+                status TEXT NOT NULL,
+                progress_details TEXT,
+                notes TEXT,
+                FOREIGN KEY (habit_id) REFERENCES habits(id)
+            )
+        ''')
+        print("Created table 'records'")
 
-    # c.execute(```
-    #     CREATE TABLE IF NOT EXISTS habits (
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         name TEXT NOT NULL,
-    #         tracking-method TEXT NOT NULL,
-    #         tracking-value TEXT NOT NULL,
-    #         tracking-frequency TEXT NOT NULL,
-    #         tracking-details TEXT NOT NULL,
-    #         start-date TEXT
-    #     )          
-    # ```)
+        habits = [
+            ('Wake up at 7am', 'at_time', '7am', 'Wake up early in the morning', '2025-01-01','daily'),
+            ('Work out 60mins', 'for_time', '60mins','Daily workout routine', '2025-01-01','daily'),
+            ('4 Boil Eggs for Breakfast', 'for_qty', '4', 'Maintain a healthy diet', '2025-01-01','daily'),
+            ('Writing for 30mins','for_time','30mins','De-clutter thinking for insights', '2025-01-01','daily'),
+            ('Drink 8 glasses of water','for_qty','8','Stay hydrated','2025-01-01','daily')
+        ]
 
-    # c.execute(```
-    #     CREATE TABLE IF NOT EXISTS record (
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         date
-    #         habits-id # foreign key from 
-    #         status TEXT NOT NULL
-    #         progress-details
-    #         user-notes TEXT      
-    #     )
-    # ```)
+        records = [
+            (1, '2025-01-01', 'done', '', 'Woke up on time')
+        ]
 
-    habits = [
-        ('Wake up at 7am', 'at_time', '7am', 0, 'Wake up early in the morning'),
-        ('Work out 60mins', 'for_time', '60mins', 0, 'Daily workout routine'),
-        ('4 Boil Eggs for Breakfast', 'for_qty', '4', 0, 'Maintain a healthy diet')
-    ]
+        c.executemany('INSERT INTO habits (name, type, value, description, start_date, frequency) VALUES (?,?,?,?,?,?)', habits)
+        print("Inserted data into 'habits'")
 
-# Scheme 
-# HABITS
-# - Habit Name - TEXT     
-# - Type > Tracking method ( type of tracking ) - TEXT
-# - Tracking value
-# - START DATE
-# - FREQUENCY
-#   - DAILY 
-#   - WEEKLY ( CUSTOM - SUN TO SAT ) 
-#   - MONTHLY ( DATE 1 TO 30 )
-#   ?? SPECIFIC DATE
-#
-# RECORD
-# - ID
-# - DATE - 2025-01-01
-# - ACTIVITY ID ( Foreign ID )
-# - STATUS - NOT STARTED / PROGRESS / DONE
-# - PROGRESS DETAILS
-#    - if Timer > 50m24s
-#    - if Quantity > 5 times
-#    - if Done or not done ... ( empty ) 
-# - NOTES (?) 
+        c.executemany('INSERT INTO records (habit_id, date, status, progress_details, notes) VALUES (?,?,?,?,?)', records)
+        print("Inserted data into 'records'")
 
-# AMD / Google / AMAZON /
-    c.executemany('INSERT INTO habits (name, type, value, done, description) VALUES (?,?,?,?,?)', habits)
-    conn.commit()
-    conn.close()
+        conn.commit()
+    finally:
+        conn.close()
 
 if __name__ == "__main__":
     init_db()
-    
